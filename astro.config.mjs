15725da7fab3cb3a10761d7dc77059d990aac0b7
Juanjo <jjromero1173@gmail.com>
2026-07-13 12:34:18 +0200

    Prepara el despliegue en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: isGitHubPages ? 'https://romani1173.github.io' : 'http://localhost:4321',
+	base: isGitHubPages ? '/porfolio-astrofoto' : '/',
 	integrations: [mdx(), sitemap()],
 });
